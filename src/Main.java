092cf56898ee5bcd2618524e561ce3c7e88203f3
Gevorg <admiralmakarov222@yandex.ru>
2024-04-07 19:36:47 +0300

    Changed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int price = 15111; // стоимость билета
+        int mile = 20; // количество рублей за одну милю
+
+        int quantity = price / mile;
+        System.out.println("Количество начисленных миль за купленный билет: " + quantity);
     }
-
-
-    
 }
